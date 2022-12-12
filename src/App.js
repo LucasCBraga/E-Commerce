5200cce4da644d316dad424756b2ba14737c4a4b
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-12 17:10:37 -0300

    terminando header

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import avatar from "./images/image-avatar.png"
 function Header() {
   return (
     
-      <header className="flex items-center justify-between p-8 border-b border-slate 400 ">
-      <div> 
+      <header className="flex items-center justify-between p-8 border-b border-slate 400 max-w-7xl mx-auto">
+      <div className="flex items-center justify-start gap-4 "> 
         <img src={logo} alt=""></img>
 
         <nav>
-          <ul>
+          <ul className="flex items-center justify-start gap-4 ">
             <li>Collections</li>
             <li>Men</li>
             <li>Women</li>
@@ -22,11 +22,11 @@ function Header() {
         </nav>
       </div>
       <div>
-        <ul>
+        <ul className="flex items-center justify-start gap-4 ">
           <li><button>
             <AiOutlineShoppingCart/>
             </button></li>
-          <li><img src={avatar} alt=""></img></li>
+          <li><img src={avatar} alt="" className="w-12"></img></li>
         </ul>
       </div>
       </header>
