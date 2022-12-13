c10966d81cd9e421c9c7e2f129bc71a4cf8efb94
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-13 14:34:32 -0300

    atualizao nos thumbnail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState } from "react"
 import { data } from "./data"
 import logo from "./images/logo.svg"
-import {AiOutlineShoppingCart} from "react-icons/ai"
+import { AiOutlineShoppingCart } from "react-icons/ai"
 import avatar from "./images/image-avatar.png"
 
 function Header() {
   return (
-    
-      <header className="flex items-center justify-between p-8 border-b border-slate 400 max-w-7xl mx-auto">
-      <div className="flex items-center justify-start gap-4 "> 
+
+    <header className="flex items-center justify-between p-8 border-b border-slate 400 max-w-7xl mx-auto">
+      <div className="flex items-center justify-start gap-4 ">
         <img src={logo} alt=""></img>
 
         <nav>
@@ -24,36 +24,44 @@ function Header() {
       <div>
         <ul className="flex items-center justify-start gap-4 ">
           <li><button>
-            <AiOutlineShoppingCart/>
-            </button></li>
+            <AiOutlineShoppingCart />
+          </button></li>
           <li><img src={avatar} alt="" className="w-12"></img></li>
         </ul>
       </div>
-      </header>
-    
+    </header>
+
   )
 }
 
 function App() {
   const [products] = useState(data)
+  const [value, setValue] = useState(0)
+
+  const { mainImage } = products[value]
+
 
   return (
     <>
       <Header />
 
-      <section>
-        {products.map((item)=>(
-          <article key={item.id}>
-            <img src={item.mainImage} alt=""/>
-            <ul>
-              <img src={item.thumbnail} alt=""/>
-            </ul>
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols2 gap-10">
+        <article>
+          <img src={mainImage} alt="" className="w-9/12 rounded-2xl" />
 
-          </article>
-        ))}
-
+          <ul className="flex items-center justify-start gap-3 flex-wrap">
+          {products.map((item, index) => (
+             <li key={item.id} onClick={() => setValue(index)}>
+                <img src={item.thumbnail} alt="" className="w-20" />
+              </li>
+          ))}
+          </ul>
+        </article>
+        
+        <article>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+        </article>
       </section>
-
     </>
   );
 }
