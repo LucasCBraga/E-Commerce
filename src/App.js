77501fcbaae5f728d6b33e60dc22e09773a39cde
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-13 15:13:24 -0300

    terminado os produtos

--- src/App.js
+++ src/App.js
@@ -45,14 +45,17 @@ function App() {
     <>
       <Header />
 
-      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
         <article>
-          <img src={mainImage} alt="" className="w-9/12 rounded-2xl" />
+          <img src={mainImage} alt="" className="w-11/12 rounded-2xl" />
 
           <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
           {products.map((item, index) => (
-             <li key={item.id} onClick={() => setValue(index)}>
-                <img src={item.thumbnail} alt="" className="w-20 rounded-xl" />
+             <li key={item.id} onClick={() => setValue(index)} className={
+              `${index === value && "border-2 border-orange-400 opacity-80"} 
+              border-2 rounded-2xl overflow-hidden cursor-pointer`}
+              >
+                <img src={item.thumbnail} alt="" className="w-20" />
               </li>
           ))}
           </ul>
