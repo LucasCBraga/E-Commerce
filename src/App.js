884565da6a3a08271eb4ffade52d9283e765f94d
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-13 15:33:19 -0300

    lista de precos

--- src/App.js
+++ src/App.js
@@ -50,19 +50,38 @@ function App() {
           <img src={mainImage} alt="" className="w-11/12 rounded-2xl" />
 
           <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
-          {products.map((item, index) => (
-             <li key={item.id} onClick={() => setValue(index)} className={
-              `${index === value && "border-2 border-orange-400 opacity-80"} 
+            {products.map((item, index) => (
+              <li key={item.id} onClick={() => setValue(index)} className={
+                `${index === value && "border-2 border-orange-400 opacity-80"} 
               border-2 rounded-2xl overflow-hidden cursor-pointer`}
               >
                 <img src={item.thumbnail} alt="" className="w-20" />
               </li>
-          ))}
+            ))}
           </ul>
         </article>
 
         <article>
-          <h1>Lorem ipsum dolor sit amet.</h1>
+          <h2>Sneakers Company</h2>
+          <h1>Fall Limited Edition Sneakers</h1>
+          <p>These low-profile sneakers are your perfect casual wear companion. Featuring a
+            durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+            <div>
+            <ul>
+              <li>$125.00</li>
+              <li>50%</li>
+            </ul>
+
+            <p><s>$250.00</s></p>
+            </div>
+            <div>
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+
+            </div>
         </article>
       </section>
     </>
