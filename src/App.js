28dfc54f66bd87816d05759ee79ad78b13246c0b
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-14 15:56:15 -0300

    estilizando botao de add to cart

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
 
       <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
         <article>
-          <img src={mainImage} alt="" className="w-11/12 rounded-2xl" />
+          <img src={mainImage} alt="" className="w-full rounded-2xl" />
 
           <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
             {products.map((item, index) => (
@@ -65,33 +65,33 @@ function App() {
         </article>
 
         <article>
-          <h2>Sneakers Company</h2>
-          <h1>Fall Limited Edition Sneakers</h1>
-          <p>These low-profile sneakers are your perfect casual wear companion. Featuring a
+          <h2 className="bg-slate-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-small font-bold inline-block rounded shadow mb-10">Sneaker Company</h2>
+          <h1 className="text-slate-900 mb-10 font-bold text-3xl lg:text-4xl ">Fall Limited Edition Sneakers</h1>
+          <p className="text-slate-600 mb-10 leading-relaxed">These low-profile sneakers are your perfect casual wear companion. Featuring a
             durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-          <div>
-            <ul>
-              <li>$125.00</li>
-              <li>50%</li>
+          <div className="flex flex-wrap items-center justify-between">
+            <ul className="flex items-center gap-5">
+              <li className="text-slate-900 font-bold text-xl ">$125.00</li>
+              <li className="bg-orange-100 py-1 px-2 text-orange-400 tracking-wide text-small font-bold inline-block rounded shadow">50%</li>
             </ul>
 
-            <p><s>$250.00</s></p>
+            <p className="text-slate-600 text-sm"><s>$250.00</s></p>
           </div>
-          <div>
-            <ul>
+          <div className="mt-10">
+            <ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow">
               <li>
-                <img src={minus} alt=""></img>              
+                <img src={minus} alt=""></img>
               </li>
               <li>{amount}</li>
               <li>
                 <img src={plus} alt="" />
-                </li>
+              </li>
             </ul>
-
-            <button>
-            <AiOutlineShoppingCart /> Add to cart
+            <div>    
+            <button className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full" >
+              <AiOutlineShoppingCart /> Add to cart
             </button>
-
+          </div>
           </div>
         </article>
       </section>
