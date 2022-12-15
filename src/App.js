d09bf4b4413b0157f1983c5ad5ae8cc091be5279
Lucas Braga <lucascbnunes1@gmail.com>
2022-12-15 11:11:30 -0300

    troca de imagem conforme usuario clica nas setas, versao mobile

--- src/App.js
+++ src/App.js
@@ -42,8 +42,25 @@ function App() {
   const [products] = useState(data)
   const [value, setValue] = useState(0)
   const [amount, setAmount] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(1)
 
   const { mainImage } = products[value]
+
+  const nextSlide = () => {
+    if (slideIndex !== products.length) {
+      setSlideIndex(slideIndex + 1)
+    } else if (slideIndex === products.length) {
+      setSlideIndex(1)
+    }
+  }
+
+  const previousSlide = () => {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1)
+    } else if (slideIndex === 1) {
+      setSlideIndex(products.length)
+    }
+  }
 
   const handleMinus = () => { 
     setAmount(amount - 1)
@@ -56,15 +73,31 @@ function App() {
 
       <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:mt-10">
         <article>
-          <div className="relative">
-          <img src={mainImage} alt="" className="w-full lg:rounded-2xl" />
-          <ul>
-            <li><button className="bg-white rounded-full p-5 shadow absolute left-4 top-1/2 -translate-y-1/2">
-              <FaChevronLeft /></button></li>
-            <li><button  className="bg-white rounded-full p-5 shadow absolute right-4 top-1/2 -translate-y-1/2" >
-              <FaChevronRight /></button></li>
-          </ul>
-          </div>
+          <div className="lg:hidden">
+            {products.map((item, index) => (
+              <div 
+              key={index}
+              className={slideIndex === index + 1 ? "relative" : "hidden"}
+              >
+              <img src={item.mainImage} 
+              alt="" 
+              className="w-full lg:rounded-2xl cursor-pointer"
+              />
+              <ul className="lg:hidden">
+                <li>
+                  <button 
+                  onClick={previousSlide}
+                  className="bg-white rounded-full p-5 shadow absolute left-4 top-1/2 -translate-y-1/2"
+                  >
+                  <FaChevronLeft /></button></li>
+                <li>
+                  <button onClick={nextSlide} className="bg-white rounded-full p-5 shadow absolute right-4 top-1/2 -translate-y-1/2" >
+                  <FaChevronRight /></button></li>
+                   </ul>
+              </div>
+            ))}
+           </div>
+            
 
           <ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5">
             {products.map((item, index) => (
